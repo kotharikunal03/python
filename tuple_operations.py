5bfd4e440fcc384c96aaf1984148881996fecc2a
kotharikunal03 <kotharikunal03@gmail.com>
2026-03-10 22:51:30 +0530

    added set operations script

---
+++ tuple_operations.py
@@ -0,0 +1,18 @@
+t1 = (1, 2, 3)
+t2 = (4, 5)
+
+# Access
+print("First:", t1[0])
+
+# Nested tuple
+nested = (t1, t2)
+
+# Repetition
+rep = t1 * 2
+
+# Concatenation
+concat = t1 + t2
+
+print("Nested:", nested)
+print("Repetition:", rep)
+print("Concat:", concat)
